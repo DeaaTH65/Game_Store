ec81be446602dadb3ec2dff14b6f13b6bd409754
Deaath65 <deaath65@gmail.com>
2023-08-22 18:10:49 +0545

    Postgres

--- g_store/g_store/settings.py
+++ g_store/g_store/settings.py
@@ -63,11 +63,17 @@ WSGI_APPLICATION = 'g_store.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Gstore',
+       'USER': 'postgres',
+       'PASSWORD': 'Deaath@65',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
+
 
 
 # Password validation
