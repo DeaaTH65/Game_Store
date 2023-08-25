edec53833468266b87ab31633cda9b6d76e1cbd7
Deaath65 <deaath65@gmail.com>
2023-08-25 14:55:32 +0545

    model-product

--- g_store/inventory/models.py
+++ g_store/inventory/models.py
@@ -8,3 +8,16 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
