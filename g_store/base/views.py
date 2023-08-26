0b2fbf3e3825ba0720c44be2fd5b6c677afa8fd9
Deaath65 <deaath65@gmail.com>
2023-08-26 13:19:35 +0545

    messages

--- g_store/base/views.py
+++ g_store/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from inventory.models import Product
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm
+from django.contrib import messages
 
 
 
@@ -12,41 +13,44 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def login_user(request):
-	if request.method == 'POST':
-		email = request.POST['email']
-		password = request.POST['password']
-		user = authenticate(request, email=email, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect('home')
-		else:
-			return redirect('home')
-	else:
-		return render(request, 'login.html')
+def login_user(request):   
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'login.html')
 
 
 def logout_user(request):
     logout(request)
+    messages.success(request, ("You have been logged out.!"))
     return redirect('home')
 
 
 def register_user(request):
-	form = SignUpForm()
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			email = form.cleaned_data['email']
-			password = form.cleaned_data['password1']
-			user = authenticate(email=email, password=password)
-			login(request, user)
-			return redirect('home')
-		else:
-			form = SignUpForm()
-			return render(request, 'register.html', {'form':form})
-
-	return render(request, 'register.html', {'form':form})
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(email=email, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+        else:
+            form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
 
 
 def profile(request, pk):
