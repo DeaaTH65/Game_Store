eaba8fab91a3dbf019aadbcbb9d08620b541d7af
Deaath65 <deaath65@gmail.com>
2023-08-29 07:48:25 +0545

    DRF Started

--- g_store/api/views.py
+++ g_store/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/products',
+        'GET /api/products/:id',
+        'GET /api/users',
+        'GET /api/users/:id',
+        'GET /api/profiles',
+        'GET /api/profiles/:id',
+        'GET /api/purchases',
+        'GET /api/purchases/:id',
+    ]
+    return Response(routes)
--- g_store/g_store/settings.py
+++ g_store/g_store/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'base',
     'inventory',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
